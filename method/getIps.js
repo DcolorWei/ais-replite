4023998a68ceb63c087da07d789b508ef04e7e6b
unknown <corfer@yeah.net>
2025-04-18 04:26:55 +0800

    feature: get ip method

---
+++ method/getIps.js
@@ -0,0 +1,14 @@
+export async function getIps(limit) {
+    const url = env.url;
+    const token = env.token;
+    return new Promise((resolve, reject) => {
+        fetch(`${url}?token=${token}&limit=${limit}&type=0&time=&split=1&split_text=`)
+            .then(res => res.text())
+            .then(data => {
+                resolve(data.split("<br />"));
+            })
+            .catch(e => {
+                reject(e);
+            })
+    })
+}
