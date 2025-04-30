90abaf7125b194f70763c8a5286793c4495bc174
unknown <corfer@yeah.net>
2025-04-30 09:45:58 +0800

    use http to instead tcp with epress

--- manager.js
+++ manager.js
@@ -1,64 +1,57 @@
 import { getIps } from './method/getIps.js';
 import fs from 'fs';
-import { createServer } from 'net';
-const PORT = 3000;
+const PORT = 3544;
 const HOST = '127.0.0.1';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 const tasks = [];
 const records = [];
 
-// 创建TCP服务器
-const server = createServer((socket) => {
-    // 接收数据                                     
-    socket.on('data', (dta) => {
-        const { action, data } = JSON.parse(dta.toString());
-        switch (action) {
-            case 'getIp':
-                const ip = ips.pop();
-                if (!ip) return null;
-                socket.write(JSON.stringify({ ip: ip }));
-                break;
-            case 'count':
-                socket.write(JSON.stringify({ count: tasks.length }));
-                break;
-            case 'addTask':
-                if (!data.time || !data.id) {
-                    socket.write(JSON.stringify({ status: false }));
-                    return;
-                }
-                tasks.push({ id: data.id, time: data.time });
-                break;
-            case 'getTask':
-                const task = tasks.pop();
-                if (!task) return null;
-                socket.write(JSON.stringify({ id: task.id }));
-                break;
-            case 'addRecord':
-                if (!data.record) {
-                    socket.write(JSON.stringify({ status: false }));
-                    return;
-                }
-                console.log(new Date(), 'add record:', data.record);
-                records.push(data.record);
-                break;
-            default:
-                socket.write(JSON.stringify({ status: true }));
-                break;
-        }
-    });
-    socket.on('error', () => socket.destroy());
-});
+app.get("/get-ip", (req, res) => {
+    const ip = ips.pop();
+    if (!ip) return res.send("")
+    res.send(ip)
+})
+
+app.get("/count", (req, res) => {
+    res.send(tasks.length)
+})
+
+app.get("/get-task", (req, res) => {
+    const task = tasks.pop();
+    if (!task) return null;
+    res.send(String(task.id));
+})
+
+app.post("/add-tasks", (req, res) => {
+    const { tasklist } = req.body;
+    console.log(tasklist)
+    if (tasklist) tasks.push(...tasklist);
+    res.send("");
+})
+
+app.post("/add-record", (req, res) => {
+    const { record } = req.body;
+    if (record) records.push(record);
+    res.send("");
+})
 
 // 启动服务器
-server.listen(PORT, HOST);
+app.listen(PORT, HOST);
 
 const ips = await getIps(90);
 console.log(ips.length);
 setInterval(async () => {
+    if (ips.length > 500) ips.length = 0;
     const newIps = await getIps(90);
-    console.log(new Date(), newIps.length, 'update ips...');
+    console.log(new Date(), ips.length, 'update ips...');
     if (!newIps.length) return;
-    ips.length = 0;
     ips.push(...newIps);
 }, 1000 * 60); // 每分钟更新一次IP列表
 
@@ -68,4 +61,4 @@ setInterval(() => {
     if (!records.length) return;
     fs.writeFileSync(`./records/record_${Date.now()}.json`, JSON.stringify(records));
     records.length = 0;
-}, 1000 * 60);
+}, 1000 * 90);
--- master.js
+++ master.js
@@ -1,60 +1,28 @@
 import { getVesselListByArea, getVesselByVesselId } from './method/vessel.js';
 import puppeteer from 'puppeteer-extra';
 import StealthPlugin from 'puppeteer-extra-plugin-stealth';
-puppeteer.use(StealthPlugin());
-import { createConnection } from 'net';
-
+puppeteer.use(StealthPlugin({
+}));
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const env = process.env;
 const host = env.TCP_HOST || "127.0.0.1";
-const port = env.TCP_PORT || 3000;
+const port = env.TCP_PORT || 3544;
 
 (async () => {
     while (1) {
-        const count = await new Promise(async r => {
-            let count = null;
-            const client = createConnection({ port, host }, () => {
-                client.write(JSON.stringify({ action: 'count' }));
-            });
-            client.once('data', (data) => {
-                count = JSON.parse(data).count;
-                client.destroy();
-            });
-            client.once('error', () => client.destroy());
-
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            r(count);
-        });
-        console.log(new Date(), 'count:', count);
+        const count = await fetch(`http://${host}:${port}/count`).then(r => r.text());
         if (count > 50) {
-            console.log(new Date(), 'Wait for task be cleared...');
             await new Promise(resolve => setTimeout(resolve, 10000));
             continue;
         }
 
-        const ip = await new Promise(async r => {
-            let ip = null;
-            const client = createConnection({ port, host }, () => {
-                client.write(JSON.stringify({ action: 'getIp' }));
-            });
-            client.once('data', (data) => {
-                ip = JSON.parse(data).ip;
-                client.destroy();
-            });
-            client.once('error', () => client.destroy());
-            await new Promise(resolve => setTimeout(resolve, 1000));
-            r(ip);
-        })
-        if (ip === null) {
-            continue;
-        }
-        console.log(`${ip}`);
+        const ip = await fetch(`http://${host}:${port}/get-ip`).then(r => r.text());
         const browser = await puppeteer.launch({
-            executablePath: env.CHROME_PATH, headless: true,
-            args: ["--start-maximized", `--proxy-server=${ip.replace("_", ':')}`],
+            executablePath: env.CHROME_PATH, headless: false,
+            args: ["--start-maximized", `--proxy-server==${ip}`],
         }).catch(e => null);
 
         if (browser === null) {
@@ -65,6 +33,7 @@ const port = env.TCP_PORT || 3000;
 
         const shipIds = [];
         try {
+            console.log(new Date(), 'count:');
             shipIds.push(...await getVesselListByArea(browser, env.LON, env.LAT));
         } catch (e) { e }
         shipIds.sort((a, b) => {
@@ -73,16 +42,18 @@ const port = env.TCP_PORT || 3000;
             return 0;
         });
         const filteredShipIds = shipIds.filter(i => Number(i.elapsed) < 3);
+        console.log(shipIds.length, filteredShipIds.length)
         if (filteredShipIds.length === 0) {
-            await new Promise(resolve => setTimeout(resolve, 1000 * 15));
+            await new Promise(resolve => setTimeout(resolve, 100));
             continue;
         }
         const time = Date.now();
-        filteredShipIds.forEach((i) => {
-            const client = createConnection({ port, host }, () => {
-                client.write(JSON.stringify({ action: 'addTask', data: { id: i.id, time: time } }));
-            });
-            client.on('error', () => client.destroy());
-        });
+        await fetch(`http://${host}:${port}/add-tasks`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ tasklist: filteredShipIds.map(i => { return { id: i.id, time: time } }) })
+        })
     }
 })();
